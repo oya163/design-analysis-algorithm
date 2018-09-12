da9fd4a41d543e7ca1fa95738d96d5cdb76f633e
oya163 <oyeshsin@hotmail.com>
2018-09-12 15:22:58 -0400

    Final touch

--- coin-exchange-greedy.py
+++ coin-exchange-greedy.py
@@ -1,7 +1,8 @@
 '''
-    Binary Loose Change
-    Date - 09/12/2018
     CMSC 641 Fall 2018
+    HW2 - Binary Loose Change
+    Due Date - 09/18/2018
+
     Greedy Algorithm
 
     This algorithm finds the minimum number of coins
@@ -11,15 +12,23 @@
 import math
 
 
-def coin_change(N, count):
-    val = int(math.log(N, 2))
-    coin = math.pow(2, math.ceil(val))
-    N = N - coin
+def coin_change(n, count):
+    val = int(math.log(n, 2))
+    coin = int(math.pow(2, math.ceil(val)))
+    print(coin)
+    n = n - coin
     count = count + 1
-    if N == 0:
+    if n == 0:
         return count
     else:
-        return coin_change(N, count)
+        return coin_change(n, count)
 
 
-print('Minimum number of coins required for exchange = ', coin_change(43, 0))
+def main():
+    init_count = 0
+    n = int(input("Enter a number: "))
+    print('Minimum number of coins required for exchange = ', coin_change(n, init_count))
+
+
+if __name__ == '__main__':
+    main()
